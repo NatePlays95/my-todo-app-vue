da69594de71dc9ff68a12cf77d7fefc93f6c8f23
Natan Maia <natanmaiaplays@outlook.com>
2024-10-27 23:16:04 -0300

    repo created

---
+++ src/TodoListApp.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello World!',
+      newTask: "",
+      taskList: ["task1", "task2"]
+    }
+  },
+
+  methods: {
+    addTask() {
+      if (this.newTask.trim()) {
+        this.taskList.push(this.newTask);
+        this.newTask = "";
+      }
+    },
+    deleteTask(index) {
+      this.taskList.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>To-Do List</h1>
+  <div>
+    <input v-model="newTask">
+    <button @click="addTask()">Add</button>
+  </div>
+
+  <ul>
+    <li v-for="(task, index) in taskList">
+      {{task}}
+      <button v-on:click="deleteTask(index)">Delete</button>
+    </li>
+  </ul>
+</template>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import './assets/main.css'
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+
+import { createApp } from 'vue'
+import App from './TodoListApp.vue'
+
+createApp(App).mount('#app')
