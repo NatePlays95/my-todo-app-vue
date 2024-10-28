3d6bf0269388c4cd27d27d6874b28c66a7008b76
Natan Maia <natanmaiaplays@outlook.com>
2024-10-28 19:27:02 -0300

    styles

--- src/TodoListApp.vue
+++ src/TodoListApp.vue
@@ -1,66 +1,84 @@
 <script>
+import './TodoListApp.css'
+import TaskCard from './components/TaskCard.vue'
+
 export default {
-  data() {
-    return {
-      newTask: "",
-      taskList: []
-    }
-  },
+    components: {
+        TaskCard
+    },
 
-  mounted() { //this runs when the page (or component) loads 
-    this.loadTaskList();
-  },
+    data() {
+        return {
+        newTask: "",
+        taskList: []
+        }
+    },
 
-  methods: {
-    addTask() {
-      if (this.newTask.trim()) {
-        this.taskList.push(this.newTask);
-        this.newTask = "";
+    mounted() { //this runs when the page (or component) loads 
+        this.loadTaskList();
+    },
+
+    methods: {
+        addTask() {
+        if (this.newTask.trim()) {
+            this.taskList.push(this.newTask);
+            this.newTask = "";
+
+            this.saveTaskList();
+        }
+        },
+
+        deleteTask(index) {
+        this.taskList.splice(index, 1);
 
         this.saveTaskList();
-      }
-    },
+        },
 
-    deleteTask(index) {
-      this.taskList.splice(index, 1);
+        saveTaskList() {
+            const taskListString = JSON.stringify(this.taskList);
+            if (taskListString) {
+                localStorage.setItem("taskList", taskListString);
+                console.log("saved list: ", taskListString);
+            }
+        },
 
-      this.saveTaskList();
-    },
-
-    saveTaskList() {
-        const taskListString = JSON.stringify(this.taskList);
-        if (taskListString) {
-            localStorage.setItem("taskList", taskListString);
-            console.log("saved list: ", taskListString);
+        loadTaskList() {
+            const savedTaskListString = localStorage.getItem("taskList");
+            if (savedTaskListString) {
+                const newTaskList = JSON.parse(savedTaskListString);
+                this.taskList = [...newTaskList];
+                console.log("loaded list: ", savedTaskListString);
+            } else {
+                console.log("no data to load");
+            }
+            
         }
-    },
-
-    loadTaskList() {
-        const savedTaskListString = localStorage.getItem("taskList");
-        if (savedTaskListString) {
-            const newTaskList = JSON.parse(savedTaskListString);
-            this.taskList = [...newTaskList];
-            console.log("loaded list: ", savedTaskListString);
-        } else {
-            console.log("no data to load");
-        }
-        
     }
-  }
 }
 </script>
 
 <template>
-  <h1>To-Do List</h1>
-  <div>
-    <input v-model="newTask">
-    <button @click="addTask()">Add</button>
-  </div>
+    <div className="app-container">
+        <div className="task-column">
+            <h2>To-Do List</h2>
 
-  <ul>
-    <li v-for="(task, index) in taskList">
-      {{task}}
-      <button v-on:click="deleteTask(index)">Delete</button>
-    </li>
-  </ul>
+            <TaskCard 
+            v-for="(task, index) in taskList"
+            :key="index"
+            :task="task"
+            :index="index"
+            @delete-task="deleteTask"
+            ></TaskCard>
+
+           
+            <input 
+            v-model="newTask" 
+            className='add-task-input' 
+            type="text"
+            placeholder="Add a new task"
+            >
+            <button className='add-task-button' @click="addTask">Add Task</button>
+            
+        </div>
+    </div>
 </template>
---
+++ src/components/TaskCard.vue
@@ -0,0 +1,28 @@
+<script>
+import './TaskCard.css'
+
+export default {
+    props: {
+        task: String,
+        index: Number
+    },
+
+    methods: {
+        handleDelete() {
+            this.$emit("delete-task", this.index);
+        }
+    }
+};
+
+</script>
+
+<template>
+    <div className="task-card">
+        {{ task }}
+
+        <button 
+        className="task-card-delete-btn"
+        @click="handleDelete"
+        >Delete</button>
+    </div>
+</template>
